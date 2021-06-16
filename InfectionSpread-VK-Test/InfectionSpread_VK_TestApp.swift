f6931e5f1310de4ad797afc7e060f4f6d865851c
Георгий <71693183+ronimizy@users.noreply.github.com>
2021-06-16 15:21:16 +0300

    Initial Commit

---
+++ InfectionSpread-VK-Test/InfectionSpread_VK_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfectionSpread_VK_TestApp.swift
+//  InfectionSpread-VK-Test
+//
+//  Created by Георгий Круглов on 16.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct InfectionSpread_VK_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
